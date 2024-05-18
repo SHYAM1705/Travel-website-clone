18b9f81e22cb01af37b3cfb20175238d19939cd7
Shyam Saad <birlashyam16@gmail.com>
2024-05-18 17:08:14 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i = 200; i++) 
+    {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+};
